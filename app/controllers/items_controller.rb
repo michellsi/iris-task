316517aab9ad53dc6a3feac8c919ef54b7332793
Michelle Knolly <michelleknolly@gmail.com>
2022-03-21 16:24:57 +0100

    Small change in items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,8 @@ class ItemsController < ApplicationController
     @page = params.fetch(:page, 0).to_i
 
     if params[:query].present?
-      @items = Item.search_by_everything(params[:query]).offset(@page * ITEM_PER_PAGE).limit(ITEM_PER_PAGE).order(params[:sort])
+      @items = Item.search_by_everything(params[:query]).order(params[:sort]).offset(@page * ITEM_PER_PAGE).limit(ITEM_PER_PAGE)
     else
-      @items = Item.order(params[:sort])
       @items = Item.offset(@page * ITEM_PER_PAGE).limit(ITEM_PER_PAGE).order(params[:sort])
     end
   end
